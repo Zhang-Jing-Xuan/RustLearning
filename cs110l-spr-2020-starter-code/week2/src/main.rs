8aa28a9cb544f621b5c92752f7c1933544b8a767
Zhang-Jing-Xuan <zjx617@yeah.net>
2022-06-26 21:28:34 +0800

    test end :rocket:

--- cs110l-spr-2020-starter-code/week2/src/main.rs
+++ cs110l-spr-2020-starter-code/week2/src/main.rs
@@ -1,50 +1,7 @@
-struct Node {
-    value: u32,
-    next: Option<Box<Node>>, // no null pointers in Rust!
-}
-
-pub struct LinkedList {
-    head: Option<Box<Node>>,
-    size: usize,
-}
-
-impl Node {
-    fn new(value: u32, next: Option<Box<Node>>) -> Node {
-        Node {value: value, next: next}
-    }
-}
-
-impl LinkedList {
-    pub fn new() -> LinkedList {
-        LinkedList {head: None, size: 0}
-    }
-    pub fn get_size(&self) -> usize {
-        self.size
-    }
-    // pub fn get_size(&self) -> usize {
-    //     (*self).size
-    // }
-    pub fn is_empty(&self) -> bool {
-        self.size == 0
-    }
-    // pub fn is_empty(&self) -> bool {
-    //     self.get_size() == 0
-    // }
-    pub fn push(&mut self, value: u32) {
-        let new_node = Box::new(Node::new(value, self.head.take()));
-        self.head = Some(new_node);
-        self.size += 1;
-    }
-    pub fn pop(&mut self) -> Option<u32> {
-        let node = self.head.take()?;
-        self.head = node.next;
-        self.size -= 1;
-        Some(node.value)
-    }
-}
-
-fn main(){
-    let mut list:LinkedList = LinkedList::new();
-    list.push(5);
-    println!("{}",list.is_empty());
+fn main() {
+    let s1 = String::from("hello");
+    let mut v = Vec::new();
+    v.push(s1);
+    let mut s2:&String = &v[0];
+    println!("{}", s2);
 }
--- cs110l-spr-2020-starter-code/week2/src/main_test2.rs
+++
@@ -1,9 +0,0 @@
-fn main(){
-    let mut s:String=String::from("Hello world!");
-    let s1=& mut s;
-    println!("{}",s1);
-    println!("{}",s);
-    
-    
-    // println!("{},{}",s1,s);
-}

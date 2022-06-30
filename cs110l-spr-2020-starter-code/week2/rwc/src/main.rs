483db03cb0449f3600e500f4e8d98c0bc30608f7
Zhang-Jing-Xuan <zjx617@yeah.net>
2022-06-30 17:23:14 +0800

    week2 Optional1 finished :bookmark:

--- cs110l-spr-2020-starter-code/week2/rwc/src/main.rs
+++ cs110l-spr-2020-starter-code/week2/rwc/src/main.rs
@@ -1,5 +1,20 @@
+// cargo run test.txt
 use std::env;
 use std::process;
+use std::fs::File;
+use std::io::{self, BufRead};
+fn read_file_lines(filename: &String) -> Result<Vec<String>, io::Error> {
+    // unimplemented!();
+    // Be sure to delete the #[allow(unused)] line above
+    let file = File::open(filename)?;
+    let mut lines=Vec::new();
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line?;
+        // do something with line_str
+        lines.push(line_str);
+    }
+    Ok(lines)
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,4 +24,17 @@ fn main() {
     }
     let filename = &args[1];
     // Your code here :)
+    let file=read_file_lines(filename).expect("Read file error");
+    println!("The number of lines is {}.",{file.len()});
+    let mut words_counts=0;
+    let mut chars_counts=0;
+    for line in file{
+        let words=line.split_whitespace();
+        for _word in words{
+            words_counts+=1;
+            chars_counts+=_word.len();
+        }
+    }
+    println!("The number of words is {}.",words_counts);
+    println!("The number of characters is {}.",chars_counts);
 }
